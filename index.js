aa9cc56e4c0ec828ab217749c2d4fc3337313fb1
Douglas Bergamo <douglasbergamo9556@hotmail.com>
2023-08-07 19:53:45 -0300

    primeiros comandos

---
+++ index.js
@@ -0,0 +1,5 @@
+function funcao() {
+    let x = document.getElementById("x").value;
+    let y = document.getElementById("y").value;
+    document.getElementById("ex11").innerHTML="Resultado =" + x*y;
+}
